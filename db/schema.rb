bc75999c5efcdb67bfe87741c406a6d85cda89c4
Katya Neumann <katya.neumann@me.com>
2019-10-06 15:41:52 -0700

    updated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_200219) do
+ActiveRecord::Schema.define(version: 2019_10_06_223207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "street1"
+    t.string "street2"
+    t.string "city"
+    t.integer "zipcode"
+    t.string "state"
+    t.string "country"
+    t.integer "managerId"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "fullname"
+    t.integer "number"
+    t.string "hours"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
